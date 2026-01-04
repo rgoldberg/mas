c56546c2a2f693a6432497c13335555f70ff5e78
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-01-04 15:31:34 -0500

    TODO: Attempt zsh.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Utilities/ShellCompletion.swift
+++ Sources/mas/Utilities/ShellCompletion.swift
@@ -20,7 +20,26 @@
 // 	case .fish:
 // 		"printf %sA\\n $completions"
 // 	case .zsh:
-// 		"local -ar aaa=(1 11 12 112 113 4);_describe -V '' aaa"
+// 		"""
+//
+// 		local search_term="${words[CURRENT]}"
+// 		if [[ ! "${search_term}" =~ ^[0-9]+$ && -n "${search_term}" ]]; then
+// 		    local -a ids descs
+// 		    local line id name
+// 		    while IFS= read -r line; do
+// 		        if [[ "${line}" =~ ^([0-9]+)[[:space:]]+(.+)$ ]]; then
+// 		            id="${match[1]}"
+// 		            name="${match[2]}"
+// 		            ids+=("${id}")
+// 		            descs+=("${id} -- ${name}")
+// 		        fi
+// 		    done < <("${command_name}" search "${search_term}" 2>/dev/null)
+// 		    if ((${#ids[@]} > 0)); then
+// 		        compadd -U -d descs -o nosort -l -- "${ids[@]}"
+// 		        ret=0
+// 		    fi
+// 		fi
+// 		"""
 // 	default:
 // 		""
 // 	}
