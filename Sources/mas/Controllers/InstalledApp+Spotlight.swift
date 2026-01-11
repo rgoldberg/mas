9721e5d96b5e31dec9ab449e44fb88cfb269badc
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-01-11 09:01:52 -0500

    Index in Spotlight folders that should contain App Store apps but that aren't indexed as such in Spotlight.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Controllers/InstalledApp+Spotlight.swift
+++ Sources/mas/Controllers/InstalledApp+Spotlight.swift
@@ -9,6 +9,32 @@ private import Atomics
 private import Foundation
 private import ObjectiveC
 
+private extension URL {
+	var installedAppURLs: [URL] {
+		FileManager.default // swiftformat:disable indent
+		.enumerator(at: self, includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsHiddenFiles])
+		.map { enumerator in
+			enumerator.compactMap { item in
+				guard
+					let url = item as? URL,
+					(try? url.resourceValues(forKeys: [.isDirectoryKey]).isDirectory) == true,
+					url.lastPathComponent == "Contents"
+				else {
+					return nil as URL?
+				}
+
+				enumerator.skipDescendants()
+				return
+					(try? url.appending(path: "_MASReceipt/receipt", directoryHint: .notDirectory).checkResourceIsReachable())
+					== true
+					? url.deletingLastPathComponent()
+					: nil
+			}
+		}
+		?? []
+	} // swiftformat:enable indent
+}
+
 var installedApps: [InstalledApp] {
 	get async throws {
 		try await mas.installedApps(matching: "kMDItemAppStoreAdamID LIKE '*'")
@@ -53,22 +79,52 @@ func installedApps(matching metadataQuery: String) async throws -> [InstalledApp
 
 			query.stop()
 
-			continuation.resume(
-				returning: query.results
-				.compactMap { result in // swiftformat:disable indent
-					(result as? NSMetadataItem).map { item in
-						InstalledApp(
-							adamID: item.value(forAttribute: "kMDItemAppStoreAdamID") as? ADAMID ?? 0,
-							bundleID: item.value(forAttribute: NSMetadataItemCFBundleIdentifierKey) as? String ?? "",
-							name: (item.value(forAttribute: "_kMDItemDisplayNameWithExtensions") as? String ?? "")
-							.removingSuffix(".app"),
-							path: item.value(forAttribute: NSMetadataItemPathKey) as? String ?? "",
-							version: item.value(forAttribute: NSMetadataItemVersionKey) as? String ?? "",
-						)
+			let installedApps = query.results
+			.compactMap { result in // swiftformat:disable indent
+				(result as? NSMetadataItem).map { item in
+					InstalledApp(
+						adamID: item.value(forAttribute: "kMDItemAppStoreAdamID") as? ADAMID ?? 0,
+						bundleID: item.value(forAttribute: NSMetadataItemCFBundleIdentifierKey) as? String ?? "",
+						name: (item.value(forAttribute: "_kMDItemDisplayNameWithExtensions") as? String ?? "")
+						.removingSuffix(".app"),
+						path: item.value(forAttribute: NSMetadataItemPathKey) as? String ?? "",
+						version: item.value(forAttribute: NSMetadataItemVersionKey) as? String ?? "",
+					)
+				}
+			}
+			.sorted(using: KeyPathComparator(\.name, comparator: .localizedStandard)) // swiftformat:enable indent
+
+			if !["1", "true", "yes"].contains(ProcessInfo.processInfo.environment["MAS_NO_AUTO_INDEX"]?.lowercased()) {
+				let installedAppPathSet = Set(installedApps.map(\.path))
+				for installedAppURL in applicationsFolderURLs.flatMap(\.installedAppURLs)
+				where !installedAppPathSet.contains(installedAppURL.filePath) { // swiftformat:disable:this indent
+					MAS.printer.warning(
+						"Found a likely App Store app that is not indexed in Spotlight in ",
+						installedAppURL.filePath,
+						"""
+
+
+						Indexing now, which will not complete until sometime after mas exits
+
+						Disable auto-indexing via: export MAS_NO_AUTO_INDEX=1
+						""",
+						separator: "",
+					)
+					Task {
+						do {
+							_ = try await run(
+								"/usr/bin/mdimport",
+								installedAppURL.filePath,
+								errorMessage: "Failed to index the Spotlight data for \(installedAppURL.filePath)",
+							)
+						} catch {
+							MAS.printer.error(error: error)
+						}
 					}
 				}
-				.sorted(using: KeyPathComparator(\.name, comparator: .localizedStandard)), // swiftformat:enable indent
-			)
+			}
+
+			continuation.resume(returning: installedApps)
 		}
 
 		query.start()
--- Sources/mas/Utilities/Sudo.swift
+++ Sources/mas/Utilities/Sudo.swift
@@ -17,8 +17,8 @@ func sudo(_ executableName: String, args: some Sequence<String>) throws {
 	try sudo([executablePath] + args)
 }
 
-private func sudo(_ args: some Sequence<String>) throws {
-	let cArgs = unsafe (["sudo"] + args).map { unsafe strdup($0) } // swiftformat:disable:this spaceAroundParens
+private func sudo(_ args: some Sequence<String>) throws { // swiftformat:disable:next spaceAroundParens
+	let cArgs = unsafe (["sudo", "MAS_NO_AUTO_INDEX=1"] + args).map { unsafe strdup($0) }
 	defer {
 		for unsafe cArg in unsafe cArgs {
 			unsafe free(cArg)
