b0c7a545f836156ac4791b0306ed13fd1cea5526
Ross Goldberg <484615+rgoldberg@users.noreply.github.com>
2026-06-17 16:38:04 -0400

    Use `getpwuid_r` & `getgrgid_r` instead of `getpwuid` & `getgrgid`, respectively.
    Signed-off-by: Ross Goldberg <484615+rgoldberg@users.noreply.github.com>

--- Sources/mas/Utilities/Processes/Group.swift
+++ Sources/mas/Utilities/Processes/Group.swift
@@ -9,7 +9,23 @@ internal import Darwin
 
 private extension gid_t {
 	var nameAndID: String {
-		"\(unsafe String(cString: unsafe getgrgid(self).pointee.gr_name).quoted) (\(self))"
+		let bufferLength = sysconf(_SC_GETGR_R_SIZE_MAX)
+		guard bufferLength > 0 else {
+			return "(\(self))"
+		}
+
+		var grp = unsafe group()
+		var buffer = Array(repeating: CChar(0), count: bufferLength)
+		var result = unsafe UnsafeMutablePointer<group>?.none
+		return if
+			unsafe getgrgid_r(self, &grp, &buffer, bufferLength, &result) == 0,
+			unsafe result != nil,
+			let namePtr = unsafe grp.gr_name
+		{
+			"\(unsafe String(cString: unsafe namePtr).quoted) (\(self))"
+		} else {
+			"(\(self))"
+		}
 	}
 }
 
--- Sources/mas/Utilities/Processes/User.swift
+++ Sources/mas/Utilities/Processes/User.swift
@@ -9,7 +9,23 @@ internal import Darwin
 
 private extension uid_t {
 	var nameAndID: String {
-		"\(unsafe String(cString: unsafe getpwuid(self).pointee.pw_name).quoted) (\(self))"
+		let bufferLength = sysconf(_SC_GETPW_R_SIZE_MAX)
+		guard bufferLength > 0 else {
+			return "(\(self))"
+		}
+
+		var pwd = unsafe passwd()
+		var buffer = Array(repeating: CChar(0), count: bufferLength)
+		var result = unsafe UnsafeMutablePointer<passwd>?.none
+		return if
+			unsafe getpwuid_r(self, &pwd, &buffer, bufferLength, &result) == 0,
+			unsafe result != nil,
+			let namePtr = unsafe pwd.pw_name
+		{
+			"\(unsafe String(cString: unsafe namePtr).quoted) (\(self))"
+		} else {
+			"(\(self))"
+		}
 	}
 }
 
